74726f006cc1ec2d6b7fd616e24ec82cf8f98f65
Jakub <9402720+Blatts12@users.noreply.github.com>
2021-10-07 16:32:31 +0200

    total reset, updating everything & dependency sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import "./App.css";
+
 function App() {
   return <div>Hello</div>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import App from "./App";
-import store from "./store";
-import "./normalize.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
